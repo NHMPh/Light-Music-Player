0fd32c3df1ccf8519e3cb88e21e2149796a865d4
NHMPh <minhphu49483.lop9a7@gmail.com>
2024-03-01 16:19:36 +0700

    update

--- FullscreenSpectrum.xaml.cs
+++ FullscreenSpectrum.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PuppeteerSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,16 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using YoutubeExplode.Common;
+using MessageBox = System.Windows.MessageBox;
+using Orientation = System.Windows.Controls.Orientation;
+using ProgressBar = System.Windows.Controls.ProgressBar;
 
 namespace NHMPh_music_player
 {
@@ -33,19 +39,11 @@ namespace NHMPh_music_player
                 await UpadateSpectrum();
             };
             InitializeComponent();
-            CreateSpectrumBar();
-            this.MouseDown += Window_MouseDown;
-            this.MouseMove += FullscreenSpectrum_MouseMove;          
-            this.WindowState = WindowState.Maximized;
+            CreateSpectrumBar();     
             timer.Start();
         }
 
-        private void FullscreenSpectrum_MouseMove(object sender, MouseEventArgs e)
-        {
-            setting.Opacity = 1;
-            this.MouseMove -= FullscreenSpectrum_MouseMove;
-        }
-
+       
         private async Task UpadateSpectrum()
         {
             await Task.Run(() =>
@@ -56,7 +54,7 @@ namespace NHMPh_music_player
                     {
                         int mutipler = 5000;
                         if (i < 50) mutipler = 2000;
-
+                        if (i < 10) mutipler = 600;
 
                         if (MainWindow.fbands[i] * mutipler > spectrumBars[i].Value)
                         {
@@ -79,14 +77,6 @@ namespace NHMPh_music_player
             });
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                setting.Opacity = 0;
-            this.MouseMove += FullscreenSpectrum_MouseMove;
-
-
-        }
         private void change_bars_color(object sender, RoutedEventArgs e)
         {
             System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
@@ -109,6 +99,104 @@ namespace NHMPh_music_player
             }
 
         }
+        private void change_background_url(object sender, RoutedEventArgs e)
+        {
+            thumbnail.ImageSource = new BitmapImage(new Uri(background_mod.Text));
+        }
+        private void change_background_local(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+           openFileDialog.Filter = "Image Files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return ;
+            thumbnail.ImageSource = new BitmapImage(new Uri(openFileDialog.FileName));
+
+        }
+        private void change_filter_color(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
+                if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    filter.Color = Color.FromArgb(filter.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invaild input");
+                return;
+            }
+        }
+        private void change_filter_opacity(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                filter.Color =Color.FromArgb(byte.Parse(filter_mod.Text), filter.Color.R, filter.Color.G, filter.Color.B);
+            }
+            catch
+            {
+                MessageBox.Show("Invaild input");
+                return;
+            }
+                     
+        }
+        private void change_spectrum_width(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                foreach (var bar in spectrumBars)
+                {
+                    bar.Width = float.Parse(width_mod.Text);
+                   
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invaild input");
+                return;
+            }
+
+        }
+        private void change_spectrum_height(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                foreach (var bar in spectrumBars)
+                {
+                  
+                    bar.Height = float.Parse(height_mod.Text);
+                  
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invaild input");
+                return;
+            }
+
+        }
+        private void change_spectrum_spaceing(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                foreach (var bar in spectrumBars)
+                {
+                 
+                    bar.Margin = new Thickness(0, 0, float.Parse(space_mod.Text), 0);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invaild input");
+                return;
+            }
+
+        }
         private void CreateSpectrumBar()
         {
             for (int i = 0; i < 256; i++)
@@ -119,12 +207,13 @@ namespace NHMPh_music_player
                     BorderThickness = new Thickness(0),
                     Background = new SolidColorBrush(Colors.Transparent),
                     Foreground = new SolidColorBrush(Colors.Red),
-                    Width = 7,
-                    Margin = new Thickness(0, 0, 1, 0),
+                    Width = 2,
+                    Margin = new Thickness(0, 0, 6, 0),
                     Height = 700,
                     Maximum = 1000,
                     Orientation = Orientation.Vertical,
-                    Value = 0
+                    Value = 0,
+                    
                 };
                 /*  if(i==125) progressBar.Value = 1000;
 
